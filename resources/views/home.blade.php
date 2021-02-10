79d8946394f5f1ee118cd1897496b4b81d211e00
Emilio <emilio.furnari87@gmail.com>
2021-02-10 14:09:20 +0100

    inizializzazione laravel con scss e bootstrap

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset('js/app.js')}}">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+    <h1>ciao</h1>
+  </div>
+</body>
+</html>
